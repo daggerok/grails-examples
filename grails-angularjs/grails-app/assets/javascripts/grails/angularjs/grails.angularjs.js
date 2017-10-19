83a0ec4a176d6acdbb06f2fa46f8790d67858b5a
Maksim Kostromin <daggerok@gmail.com>
2017-10-19 06:11:53 +0300

    add grails-angularjs

---
+++ grails-angularjs/grails-app/assets/javascripts/grails/angularjs/grails.angularjs.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /grails/angularjs/core/grails.angularjs.core
+//= require /grails/angularjs/index/grails.angularjs.index
+
+angular.module("grails.angularjs", [
+        "grails.angularjs.core",
+        "grails.angularjs.index"
+    ]);
---
+++ grails-angularjs/grails-app/init/grails/angularjs/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.angularjs
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
